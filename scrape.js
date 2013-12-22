5d19df067301e8fd1413d178a076438016c9c752
rserota <raphael.serota@gmail.com>
2013-12-22 16:24:16 -0700

    run along now, little doggy

--- scrape.js
+++ scrape.js
@@ -1,12 +1,11 @@
 // dependencies
-var csv = require('csv');
 var fs = require('fs');
 var request=require("request");
 var cheerio=require("cheerio");
 var writeStream = fs.createWriteStream("file.csv");
 
 // write headers to csv
-writeStream.write('Title,' + 'URL' + '\n');
+writeStream.write('Title,' + 'URL,' + 'Username,' + 'Comments' + '\n');
 
 // perfrom request
 request('https://news.ycombinator.com', function (error, response, html) {
@@ -22,8 +21,11 @@ request('https://news.ycombinator.com', function (error, response, html) {
       // parse the href attribute from the "a" element:
       var url = a.attr('href');
 
+      var username = $(this).closest('tr').next().find('a:nth-child(2)').text()
+      var comments = parseInt($(this).closest('tr').next().find('a:nth-child(3)').text().split(' ')[0])
+
       // write data to csv
-      writeStream.write(title + ',' + url + '\n');
+      writeStream.write(title + ',' + url + ',' + username + ',' + comments + '\n');
       
       // data store in an object (for dumping to mongo)
       var scrapedData = {
