1f665a9b97d847d81f0849f7fd27602ff999380a
rserota <raphael.serota@gmail.com>
2013-12-23 17:53:01 -0700

    scrapey scrape

--- scrape.js
+++ scrape.js
@@ -7,33 +7,43 @@ var writeStream = fs.createWriteStream("file.csv");
 // write headers to csv
 writeStream.write('Title,' + 'URL,' + 'Username,' + 'Comments' + '\n');
 
+var hnQueries = []
+hnPosts = []
 // perfrom request
-request('https://news.ycombinator.com', function (error, response, html) {
+request('https://angel.co/featured', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     // console.log(html)
     // pass DOM to cheerio
     var $ = cheerio.load(html);
-    $('span.comhead').each(function(i, element){
-      // select previous element
-      var a = $(this).prev();
-      // parse the link title
-      var title = a.text();
-      // parse the href attribute from the "a" element:
-      var url = a.attr('href');
+    $('.name').each(function(i,element){
+      var query = ('https://www.hnsearch.com/search#request/submissions&q=' + $(this).text() + '&sortby=create_ts+desc&start=0').replace(/\n/g,'')
+      console.log(query)
+      hnQueries.push(query)
+    })
 
-      var username = $(this).closest('tr').next().find('a:nth-child(2)').text()
-      var comments = parseInt($(this).closest('tr').next().find('a:nth-child(3)').text().split(' ')[0])
+for (var i = 0; i < hnQueries.length; i++){
+  var newQuery = hnQueries[i]
+  console.log('newquery: ',newQuery)
+  request(newQuery, function (error, response, html){
+    if (!error && response.statusCode == 200){
+      var $ = cheerio.load(html)
+      console.log(html)
+      console.log('story title: ', $('a.content-story-title'))
+    }
+  })
+}
+
 
       // write data to csv
-      writeStream.write(title + ',' + url + ',' + username + ',' + comments + '\n');
+      // writeStream.write(title + ',' + url + ',' + username + ',' + comments + '\n');
       
       // data store in an object (for dumping to mongo)
-      var scrapedData = {
-        title: title,
-        url: url
-      };
+      // var scrapedData = {
+      //   title: title,
+      //   url: url
+      // };
       //console.log(scrapedData);
-    });
+    
     console.log("\nDONE!\n")
   }
 });
